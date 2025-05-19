c4a98e57dea7dda155df13b18de9b4d3831abfb8
Akhil Bhat <akhilbhat88@yahoo.in>
2025-05-19 15:49:08 +0530

    1st Commit

--- ice-breaker.py
+++ ice-breaker.py
@@ -2,21 +2,15 @@ from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 
+from third_party.linkedin import scrape_linkedin_profile
+
 if __name__ == "__main__":
     load_dotenv()
 
     print("Hello LangChain")
-    information = """
-        Kawhi Anthony Leonard (/kəˈhwaɪ/ kə-WHY;[1] born June 29, 1991) is an American professional basketball player for the Los Angeles Clippers of the National Basketball Association (NBA). A two-time NBA champion, he is a six-time All-Star and a six-time member of the All-NBA Team (including three First Team selections). Nicknamed the "Claw" or "Klaw" for his ball-hawking skills and exceptionally large hands, Leonard is often regarded as one of the greatest[under discussion] two-way players in NBA history,[2][3][4] earning seven All-Defensive Team selections and winning Defensive Player of the Year honors in 2015 and 2016. In 2021, he was named to the NBA 75th Anniversary Team.
-
-Leonard played two seasons of college basketball for the San Diego State Aztecs and was named a consensus second-team All-American as a sophomore. He opted to forgo his final two seasons of college eligibility to enter the 2011 NBA draft. He was selected by the Indiana Pacers with the 15th overall pick before being traded to the San Antonio Spurs on draft night.
-
-With the Spurs, Leonard won an NBA championship in 2014, when he was named the Finals Most Valuable Player. After seven seasons with the Spurs, Leonard was traded to the Toronto Raptors in 2018. In 2019, he led the Raptors to their first NBA championship and won his second Finals MVP award (one of only three players to win Finals MVP with multiple teams, along with Kareem Abdul-Jabbar and LeBron James). He subsequently moved to his hometown of Los Angeles and signed with the Clippers as a free agent in July 2019. 
-
-    """
 
     summary_template = """
-    given the information {information} about a person I want you to create:
+    given the Linkedin information {information} about a person I want you to create:
     1. A short summary
     2. two interesting facts about them
     """
@@ -25,9 +19,13 @@ With the Spurs, Leonard won an NBA championship in 2014, when he was named the F
         input_variables=["information"], template=summary_template
     )
 
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
 
     chain = summary_prompt_template | llm
-    res = chain.invoke(input={"information": information})
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url="https://www.linkedin.com/in/akhil-bhat/"
+        ,mock=True
+    )
+    res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
---
+++ third_party/linkedin.py
@@ -0,0 +1,47 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
+    """scrape information from LinkedIn profiles,
+    Manually scrape the information from the LinkedIn profile"""
+
+    if mock:
+        linkedin_profile_url = "https://gist.githubusercontent.com/akhilbhat-prog/6689e54e34da3e65e91070b801385a49/raw/e49cac1df5beaec355f832e7de5d5d58e97a95d6/akhil-bhat.json"
+        response = requests.get(
+            linkedin_profile_url,
+            timeout=10,
+        )
+    else:
+        # ENROLL WITH COUPON CODE: EDENMARCO
+        # For 20% Discount on all pricing
+        api_endpoint = "https://api.scrapin.io/enrichment/profile"
+        params = {
+            "apikey": os.environ["SCRAPIN_API_KEY"],
+            "linkedInUrl": linkedin_profile_url,
+        }
+        response = requests.get(
+            api_endpoint,
+            params=params,
+            timeout=10,
+        )
+
+    data = response.json().get("person")
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None) and k not in ["certifications"]
+    }
+
+    return data
+
+
+if __name__ == "__main__":
+    print(
+        scrape_linkedin_profile(
+            linkedin_profile_url="https://www.linkedin.com/in/akhil-bhat/"
+        ,mock=True),
+    )
