870c3973b5f07a457cf7272b8b38eea1ac20527e
Akhil Bhat <akhilbhat88@yahoo.in>
2025-05-20 07:40:44 +0530

    1st Commit

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,46 +1,54 @@
+from typing import Tuple
+
 from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 
-from output_parsers import summary_parser
-from third_party.linkedin import scrape_linkedin_profile
+from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parsers import summary_parser, Summary
 
 
-def ice_break_with(name: str) -> str:
+def ice_break_with(name: str) -> Tuple[Summary, str]:
     linkedin_username = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(
         linkedin_profile_url=linkedin_username, mock=True
     )
 
     summary_template = """
-    given the information about a person from linkedin {information},
-     I want you to create:
-    1. A short summary
-    2. two interesting facts about them 
+    Given the following information about a person from LinkedIn: {information},
 
-    Use both information from twitter and Linkedin
-    \n{format_instructions}
+    Please respond in the following JSON format:
+
+    {format_instructions}
+
+    Only return JSON. Do not include any extra explanation or text.
     """
+
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"],
+        input_variables=["information", "twitter_posts"],
         template=summary_template,
         partial_variables={
             "format_instructions": summary_parser.get_format_instructions()
         },
     )
 
-    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    raw_output = (summary_prompt_template | llm).invoke(
+        {"information": linkedin_data}
+    )
+    print("Raw LLM Output:\n", raw_output)
 
     chain = summary_prompt_template | llm | summary_parser
 
     res = chain.invoke(input={"information": linkedin_data})
 
-    print(res)
+    return res, linkedin_data.get("profile_pic_url")
 
 
 if __name__ == "__main__":
     load_dotenv()
 
     print("Ice Breaker Enter")
-    ice_break_with(name="Akhil Bhat Dell")
+    ice_break_with(name="Akhil Bhat")
